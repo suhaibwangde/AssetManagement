eda4809c3c6bdce3813aa113de1d0d2b74e7fa4c
suhaib wangde <suhaib.a.wangde@gmail.com>
2018-03-03 10:33:44 -0500

    - Add Mixins - Add total Asset reducer

---
+++ AssetApp/src/reducers/totalAssetsReducer.js
@@ -0,0 +1,10 @@
+import  * as types from '../actions/actionTypes';
+import initialState from './initialState';
+export  default  function totalAssetReducer(state = initialState.totalAssets, action) {
+  switch (action.type) {
+    case types.TOTAL_ASSETS:
+      return action.totalAssets;
+    default:
+      return state;
+  }
+}
